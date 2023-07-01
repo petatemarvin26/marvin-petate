2a1acefe24440e3ceef739395fde1f11c23da9aa
Marvin <petatemarvin26@gmail.com>
2023-07-01 18:13:24 +0800

    initial parallax

--- src/navigators/Home/index.tsx
+++ src/navigators/Home/index.tsx
@@ -31,6 +31,8 @@ const HomeNavigator: React.FC = () => {
       <TechStackPage />
       <ProjectsPage />
       <ContactPage />
+
+      {/* CLOUD 1 */}
       <ParallaxLayer
         className={styles['parallax-layer-pane']}
         offset={0}
@@ -47,17 +49,37 @@ const HomeNavigator: React.FC = () => {
       >
         <CloudSvg className={styles['cloud-medium']} />
       </ParallaxLayer>
+
+      {/* CLOUD 2 */}
       <ParallaxLayer
         className={styles['parallax-layer-pane']}
         offset={0}
-        speed={-0.3}
+        factor={2}
+        speed={-0.27}
+      >
+        <CloudSvg className={styles['cloud-large2']} />
+      </ParallaxLayer>
+      <ParallaxLayer
+        className={styles['parallax-layer-pane']}
+        offset={0}
+        factor={2}
+        speed={-0.15}
+      >
+        <CloudSvg className={styles['cloud-medium2']} />
+      </ParallaxLayer>
+
+      {/* CLOUD ROCKET AND MOON */}
+      <ParallaxLayer
+        className={styles['parallax-layer-pane']}
+        offset={0}
+        speed={-0.25}
       >
         <RocketSvg className={styles['rocket']} />
       </ParallaxLayer>
       <ParallaxLayer
         className={styles['parallax-layer-pane']}
         offset={0}
-        speed={-0.15}
+        speed={-0.1}
       >
         <MoonSvg className={styles['moon']} />
       </ParallaxLayer>
