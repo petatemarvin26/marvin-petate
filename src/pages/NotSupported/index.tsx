bf5e90e2f9040dc73f5234f84074d8c1e87ddc2c
Marvin <petatemarvin26@gmail.com>
2023-07-09 23:49:03 +0800

    fixup! release/2.0.0

--- src/pages/NotSupported/index.tsx
+++ src/pages/NotSupported/index.tsx
@@ -1,9 +1,22 @@
-import {View} from 'vin-react';
+import {View, Text} from 'vin-react';
 
 import styles from './.module.css';
+import {GearSvg} from 'assets';
 
 const NotSupported: React.FC = () => {
-  return <View className={styles['not-supported-page']}>Not Supported</View>;
+  return (
+    <View className={styles['not-supported-page']}>
+      <View className={styles['top-pane']}>
+        <GearSvg className={styles['gear-svg']} />
+      </View>
+      <View className={styles['center-pane']}>
+        <Text className={styles['title-txt']}>Ow Sorry!</Text>
+        <Text className={styles['subtitle-txt']}>
+          this screen is not supported yet
+        </Text>
+      </View>
+    </View>
+  );
 };
 
 export default NotSupported;
